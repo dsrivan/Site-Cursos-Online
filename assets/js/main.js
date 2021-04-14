ac0a7ae1c92c6d6a60922de700cf34de30424eb7
Ivan da Silva Rosa <iivan.si@msn.com>
2021-04-13 23:36:52 -0300

    add bulma css e sweet alert 2

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function trySweetAlert2() {
+    Swal.fire({
+        icon: 'success',
+        title: 'Some title',
+        text: 'Something ...',
+        footer: '<a href>Link example for anything</a>'
+    })
+}
