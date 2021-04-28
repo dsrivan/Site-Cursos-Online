7ca288c3d12bd9e1f6726819096a8be45efd9651
Ivan da Silva Rosa <iivan.si@msn.com>
2021-04-27 21:49:48 -0300

    responsividade

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,15 +9,22 @@ function trySweetAlert2() {
 
 const navUl = document.querySelector('header .header .headerNav nav ul.isMobile');
 const html = document.querySelector('html');
+const headerTitles = document.querySelector('.headerTitles');
 
 const divButtonMenuMobile = document.querySelector('.divButtonMenuMobile');
 divButtonMenuMobile.addEventListener('click', () => {
     navUl.classList.toggle('showMenu');
     html.classList.toggle('is-overflow-hidden');
+    setTimeout(() => {
+        headerTitles.classList.toggle('is-hidden');
+    }, 200);
 });
 
 const divButtonMenuMobileClose = document.querySelector('.divButtonMenuMobileClose');
 divButtonMenuMobileClose.addEventListener('click', () => {
     navUl.classList.toggle('showMenu');
     html.classList.toggle('is-overflow-hidden');
+    setTimeout(() => {
+        headerTitles.classList.toggle('is-hidden');
+    }, 200);
 });
