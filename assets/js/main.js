b4856cf59b428328a5e893b69d23beaaaa0aef35
Ivan da Silva Rosa <iivan.si@msn.com>
2021-04-27 22:11:40 -0300

    responsividade e to top button

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,7 @@ function trySweetAlert2() {
 const navUl = document.querySelector('header .header .headerNav nav ul.isMobile');
 const html = document.querySelector('html');
 const headerTitles = document.querySelector('.headerTitles');
+const backToTop = document.querySelector('.backToTop');
 
 const divButtonMenuMobile = document.querySelector('.divButtonMenuMobile');
 divButtonMenuMobile.addEventListener('click', () => {
@@ -17,6 +18,7 @@ divButtonMenuMobile.addEventListener('click', () => {
     html.classList.toggle('is-overflow-hidden');
     setTimeout(() => {
         headerTitles.classList.toggle('is-hidden');
+        backToTop.classList.toggle('is-hidden');
     }, 200);
 });
 
@@ -26,5 +28,23 @@ divButtonMenuMobileClose.addEventListener('click', () => {
     html.classList.toggle('is-overflow-hidden');
     setTimeout(() => {
         headerTitles.classList.toggle('is-hidden');
+        backToTop.classList.toggle('is-hidden');
     }, 200);
 });
+
+backToTop.addEventListener('click', () => {
+    window.scroll({
+        top: 0,
+        behavior: 'smooth'
+    })
+});
+
+function fnShowHideBtnBackToTop() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        backToTop.classList.remove('is-All-Right');
+    } else {
+        backToTop.classList.add('is-All-Right');
+    }
+}
+
+window.onscroll = () => fnShowHideBtnBackToTop();
